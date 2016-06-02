f6331e60de3b15bce49efe37ec68ab34004d8dcb
Mark C.J <chenjian345@gmail.com>
2016-06-02 17:07:41 +0800

    Initial Version

---
+++ iOSRedisKit/RedisKit.h
@@ -0,0 +1,15 @@
+//
+//  RedisKit.h
+//  iOSRedisKit
+//
+//  Created by Mark C.J. on 15/12/8.
+//  Copyright © 2015年 Mark C.J. All rights reserved.
+//
+
+#ifndef RedisKit_h
+#define RedisKit_h
+
+#import "CocoaPromise.h"
+#import "CocoaRedis.h"
+
+#endif /* RedisKit_h */
